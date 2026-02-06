64fc2b2f53e5dc1ce39fee32c1fdfcac6d26fddd
“radhika301” <radhika_85@hotmail.com>
2026-02-06 14:13:46 -0500

    UPDATED MESSAGE TO FROM MAIN LOCAL

--- src/main/java/com/springboot/springsecurity/HelloController.java
+++ src/main/java/com/springboot/springsecurity/HelloController.java
@@ -23,7 +23,7 @@ public class HelloController {
     // Private endpoint (requires authentication)
     @GetMapping("/hi11")
     public String privateEndpoint1() {
-        return "Hello secure Worlddddddddd! FROM MAIN";
+        return "Hello secure Worlddddddddd! FROM MAIN LOCAL";
     }
 
 }
