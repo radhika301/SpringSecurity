96d7dfa4cdf713aafb26520983cdb8a603dd39f2
“radhika301” <radhika_85@hotmail.com>
2026-02-06 18:39:07 -0500

    updated hello controller message to "Hello secure Worlddddddddd from MAIN LOCAL! check merge conflict";

--- src/main/java/com/springboot/springsecurity/HelloController.java
+++ src/main/java/com/springboot/springsecurity/HelloController.java
@@ -17,7 +17,7 @@ public class HelloController {
     // Private endpoint (requires authentication)
     @GetMapping("/hello")
     public String privateEndpoint() {
-        return "Hello secure Worlddddddddd from MAIN LOCAL!";
+        return "Hello secure Worlddddddddd from MAIN LOCAL! check merge conflict";
     }
     
     // Private endpoint (requires authentication)
